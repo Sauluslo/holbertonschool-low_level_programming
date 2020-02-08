2353c16d58ea5f6e62a13fd6d2613c8affdc1b86
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-08 04:24:22 +0000

    Last Digitc

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5.\n", n, t);
 	}
-	else if (t < 6 && t != 0)
+	if (t < 6 && t != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0.\n", n, t);
 	}
