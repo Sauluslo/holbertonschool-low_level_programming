f5fc7a2a7181d424048a98a4c8d18843a65003e0
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-08 08:27:39 +0000

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 
 	if (t == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, t);
+		printf("Last digit of %i is %i and is 0\n", n, t);
 	}
 	else if (t < 6 && t != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, t);
 	}
 	if (t > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, t);
+		printf("Last digit of %i is %i and is greater than 5\n", n, t);
 	}
 	return (0);
 }
