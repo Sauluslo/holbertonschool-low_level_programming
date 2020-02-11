d4a1f73dd57272052c23f17717f4309752921789
Sauluslo <Aldebaranlopera14@gmail.com>
2020-02-11 15:00:00 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char hol[] = "Holberton";
 	int i;
 
-	for(i = 0; hol[i] != '\0'; i++)
+	for (i = 0; hol[i] != '\0'; i++)
 	{
 		_putchar(hol[i]);
 	}
 	_putchar('\n');
-	return(0);
 }
