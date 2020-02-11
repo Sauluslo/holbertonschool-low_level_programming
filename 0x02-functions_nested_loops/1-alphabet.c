f6cb565c4b6d3c3cc0d4039e73c6f95962f0c765
Sauluslo <Aldebaranlopera14@gmail.com>
2020-02-11 15:11:56 +0000

    And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -11,9 +11,9 @@ void print_alphabet(void)
 
 	char moe;
 
-	for(moe = a; moe <= b; moe++)
+	for (moe = a; moe <= b; moe++)
 	{
-		_putchar(moe);		
+		_putchar(moe);
 	}
 	_putchar('\n');
 }
