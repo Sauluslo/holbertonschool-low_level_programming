5999d4d89a4676f3e45a275a46ef357c05b740e0
Sauluslo <Aldebaranlopera14@gmail.com>
2020-02-11 15:41:26 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _islower - check the code for Holberton School students.
+ * @c: is the lyric c
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
