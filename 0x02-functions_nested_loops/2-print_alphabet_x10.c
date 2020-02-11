c31f23f12890151770e24f85cda3b001b3581d74
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-11 05:31:56 +0000

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	char b = 'z';
+
+	int l;
+	char moe;
+
+	for(l = 0; l <= 9; l++)
+	{
+		for(moe = a; moe <= b; moe++)
+		{	
+			_putchar(moe);
+		}
+	_putchar('\n');
+	}
+}
