746f52c2a09aa10925f8c550f641c04de5f238dc
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-12 02:41:12 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - check the code for Holberton School students.
+ * @y: int 1
+ * @z: int 2
+ * Return: Always 0.
+ */
+int add(int y, int z)
+{
+	int x;
+
+	x = y + z;
+	return (x);
+}
