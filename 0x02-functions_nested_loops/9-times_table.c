74385b11fd2cda833ee7f02110fdbf97e2bb7963
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-12 04:33:37 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int d;
+	int u;
+	int o;
+
+	for (d = 0; d <= 9; d++)
+	{
+		for (u = 0; u <= 9; u++)
+		{
+			o = d * u;
+
+			if (u > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (o < 10)
+				{
+					_putchar(' ');
+				}
+			}
+
+			if (o >= 10)
+			{
+				_putchar(o / 10 + '0');
+				_putchar(o % 10 + '0');
+			}
+			else
+			{
+				_putchar(o + '0');
+			}
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
