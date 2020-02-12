3b8993c82de0bbe4a07560be98dbfd9b17236b62
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-12 00:00:21 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * jack_bauser - check the code for Holberton School students.
- * 
+ * jack_bauer - check the code for Holberton School students.
+ *
  * Return: Always 0.
  */
 void jack_bauer(void)
