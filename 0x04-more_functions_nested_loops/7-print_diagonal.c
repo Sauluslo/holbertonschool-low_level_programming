1359af880849623333504646636c51f19ee7f82e
Sauluslo <Aldebaranlopera14@gmail.com>
2020-02-13 21:39:51 +0000

    7. I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ * @n: is the number of times the character
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int d;
+	int x;
+
+	if (n > 0)
+	{
+		for (d = 0; d < n; d++)
+		{	for (x = 0; x <= d; x++)
+			{
+				if (x != d)
+					_putchar(' ');
+				else
+					_putchar(92);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
