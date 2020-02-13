54b46fc4fbedd335dc52685d2509ba67bd4db741
Sauluslo <Aldebaranlopera14@gmail.com>
2020-02-13 15:13:01 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isupper - check the code for Holberton School students.
+ * @c: is the lyric C
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c == 67)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar(c);
+	_putchar('\n');
+}
