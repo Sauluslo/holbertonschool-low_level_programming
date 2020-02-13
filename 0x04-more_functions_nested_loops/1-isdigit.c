deec473868b92eb65bbc5cbf684f1dbeb028d879
Sauluslo <Aldebaranlopera14@gmail.com>
2020-02-13 18:42:58 +0000

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ * @c: the character c
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
