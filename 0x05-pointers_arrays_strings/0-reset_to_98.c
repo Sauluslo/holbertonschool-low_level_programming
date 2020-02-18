4036590d71b2e0e70dbd13f26d49945691090bcd
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-02-18 16:13:51 +0000

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * reset_to_98 - check the code for Holberton School students.
- * @*n: Pointer of integer
+ * @n: Pointer of integer
+ *
  * Return: Always 0.
  */
 void reset_to_98(int *n)
