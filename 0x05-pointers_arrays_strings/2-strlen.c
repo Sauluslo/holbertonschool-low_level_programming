5d798b0c732c38e5ec4069a446634d2f973ef57b
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-02-18 04:49:01 +0000

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @*s: Pointer s.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int v;
+
+	for(v = 0; s[v] != '\0'; v++)
+	{
+	}
+	return (v);
+}
