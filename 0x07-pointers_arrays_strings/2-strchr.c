7786db89bdafcfa344b56b26323311e4bdd289ad
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-02-24 21:19:16 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - check the code for Holberton School students.
+ * @s: value char 3
+ * @c: value char 2
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int count;
+
+	for(count = 0; s[count] != '\0'; count++)
+	{
+		if(s[count] == c)
+			return (&s[count]);
+	}
+	return (&s[count]);
+}
