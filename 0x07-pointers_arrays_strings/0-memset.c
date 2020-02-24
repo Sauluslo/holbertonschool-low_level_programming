9cce0821c145c6556184ff9cf8ae5756cd06dd98
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-02-24 19:05:36 +0000

    0. memset and README.md

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * *_memset - The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b.
+ * *_memset - The _memset() function fills the first
+ * n bytes of the memory area pointed to by s with the constant byte b.
  * @s: the address of memory to print
  * @b: value of caracter for change
  * @n: value unsigned integer
@@ -13,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count;
 
-	for(count = 0; count < n; count++)
+	for (count = 0; count < n; count++)
 	{
 		s[count] = b;
 	}
