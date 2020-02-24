c958cb9832f0ae34a984a97dd7c0fb86b631c029
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-02-24 23:15:28 +0000

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * strspn - Returns the number of bytes in the initial
+ * segment of s which consist only of bytes from accept
+ * @s: string char.
+ * @accept: string char.
+ *
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c1;
+	unsigned int c2;
+	unsigned int u;
+
+	for (c1 = 0; s[c1] != ' '; c1++)
+	{
+		for (c2 = 0; accept[c2] != '\0'; c2++)
+		{
+			if (s[c1] == accept[c2])
+				u++;
+		}
+	}
+	return (u);
+}
