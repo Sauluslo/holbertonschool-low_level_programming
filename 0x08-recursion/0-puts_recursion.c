30909bc8dc054e45fd8214bfd0bb363bcab6d6af
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-02-26 20:57:03 +0000

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - The standard library provides a similar function: 
+ * puts. Run man puts to learn more.
+ * @s: string a recorrer.
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar(s);
+}
