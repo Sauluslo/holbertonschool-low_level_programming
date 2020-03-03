83c4ea7cd0896eb3afe68571919df32a46512d98
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-03 20:43:38 +0000

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,51 +1,28 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * create_array - prints buffer in hexa
+ * @size: the address of memory to print
+ * @c: the size of the memory to print
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
+	char *s;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *buffer;
-
-	buffer = create_array(98, 'H');
-	if  (buffer == NULL)
+	if (size == 0)
 	{
-		printf("failed to allocate memory\n");
-		return (1);
+		return(NULL);
 	}
-	simple_print_buffer(buffer, 98);
-	free(buffer);
-	return (0);
+
+	s = (char *)malloc(sizeof(c)*size);
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;	
+	}
+	return (s);
 }
