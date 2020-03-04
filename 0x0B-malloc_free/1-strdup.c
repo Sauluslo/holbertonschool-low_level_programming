a0afbcb70280cfe86f40a4a7c00930cc8a70a41b
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-04 22:20:48 +0000

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+int _strlen(char *a);
+/**
+ * strlen - check the code for Holberton School students.
+ * @a: array a contar
+ * Return: Always 0.
+ */
+int _strlen(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * strdup - check the code for Holberton School students.
+ * @str: array value
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int x;
+
+	x = _strlen(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(char) * x + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		s[x] = str[x];
+	}
+	s[x] = '\0';
+	return (s);
+}
