496e61b027dbd91c35a12726ac1d49e8cfcc2a08
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-03-06 03:39:57 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - a function that allocates memory using malloc.
+ * @nmemb: the address of memory to print
+ * @size: integer size.
+ *
+ * Return: Returns a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int count;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (count = 0; count < nmemb * size; count++)
+	{
+		p[count] = 0;
+	}
+	return (p);
+}
