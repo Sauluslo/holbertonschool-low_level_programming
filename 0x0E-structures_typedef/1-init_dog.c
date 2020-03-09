919f37297408a4af4f827ec019e7810c57105297
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-09 22:06:43 +0000

    1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - program that prints file name.
+ *
+ * Return: 0 always.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != '\0')
+	{
+		d = 0;
+	}
+	return (0);
+}
