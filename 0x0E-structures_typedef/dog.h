11a4177c53c52bb7ce72273f39329f0d3ad49ea3
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-09 21:49:38 +0000

    0. Django

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_HOL
 #define DOG_HOL
+/**
+ * struct dog - Structure contain name, age owner of my dog.
+ * @name: Name of my dog.
+ * @owner: Name.
+ * @age: Age of my dog.
+ *
+*/
 struct dog
 {
 	char *name, *owner;
