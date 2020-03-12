6bb73cd9ffa7fd08e1b06d2fdfa8439f89957f71
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-03-12 03:49:11 +0000

    this is program execute to Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - function pointer to print name.
+ * @name: type char of pointer.
+ * @f: pointer to function.
+ *
+ * Return: Print name Always successful.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
