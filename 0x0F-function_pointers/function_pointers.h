7f18d9ff6e6e8c11c6ee102513a28bf3c73a81dd
Aldebaran Lopera <Aldebaranlopera14@gmail.com>
2020-03-12 03:53:11 +0000

    File Header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,17 +1,8 @@
-#ifndef DOG_HOL
-#define DOG_HOL
-/**
- * struct dog - Structure contain name, age owner of my dog.
- * @name: Name of my dog.
- * @owner: Name.
- * @age: Age of my dog.
- *
-*/
-typedef struct dog
-{
-	char *name, *owner;
-	float age;
-};
+#ifndef FUNC_POIN
+#define FUNC_POIN
+
+int _putchar(char c);
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
