de3bd6dc46c304c0c061fe4fa0128f0259b6d649
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-19 20:18:55 +0000

    Files 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,34 @@
+#ifndef LIST_HOL
+#define LIST_HOL
+/**
+ * Libreries
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * Prototypes of functios
+ */
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
+
