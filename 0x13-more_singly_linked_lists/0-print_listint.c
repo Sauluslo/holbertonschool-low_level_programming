d41d0edf5230a84126d4da05b9f52c1dc0916892
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-24 09:47:10 +0000

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_list - function that prints all the elements of a list_t list.
+ * print_listint - function that prints all the elements of a list_t list.
  * @h: pointer to print list.
  *
  * Return: the number of nodes.
