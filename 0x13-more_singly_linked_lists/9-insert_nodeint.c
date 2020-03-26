7766ca196abf78e51941f89960daa64365782542
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-26 02:01:54 +0000

    9. Insert new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	temp = *head;
 	if (idx == 0)
 	{
