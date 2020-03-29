efdacd6ae34068d96763673fbda5f5db8d60c0ff
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-29 03:55:01 +0000

    File Readme, putchar and file header

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int.
+ * @b: string of 0 and 1.
+ *
+ * Return: unsigned int.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base = 1, n = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	while (i)
+	{
+		n += (b[i - 1] - '0') * base;
+		base *= 2;
+		i--;
+	}
+	return (n);
+}
