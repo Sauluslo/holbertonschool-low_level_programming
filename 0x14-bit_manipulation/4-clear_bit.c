259c3562fe52c76af311d8c7f362c7085f52e076
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-03-29 04:36:31 +0000

    function that sets the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * clear_bit - function that prints the binary representation of a number.
+ * @n: unsigned long int type.
+ * @index: unsigned int.
+ *
+ * Return: always successful.
+*/
+int clear_bit(unsigned long int *n, unsigned int index);
+{
+	unsigned long int tru = 1;
+	unsigned int size = sizeof(n) * 8;
+
+	if (*n == '\0')
+		return (-1);
+	if (index > size)
+		return (-1);
+	tru <<= index;
+	*n &= ~tru;
+	return (1);
+}
