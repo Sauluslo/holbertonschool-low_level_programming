957e1d0136da192534dfd5e5de2aa7f8f0270bf4
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-05-04 01:33:38 +0000

    fix betty line 3wq

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint - function that adds a new node at the beginning 
+ * add_dnodeint - function that adds a new node at the beginning
  * of a dlistint_t list.
  * @head: pointer head of the DLL
  * @n: const int
