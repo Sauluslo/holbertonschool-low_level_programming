4dff6f095101993b2db6739823df33289c522194
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-05-30 02:15:44 +0000

    5 and 6

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,34 +2,32 @@
 /**
  * hash_table_print - function that prints a hash table.
  * @ht: hash table to be printed.
- * 
+ *
  * Return: Printed elements of the hash table.
 */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index = 0;
-    unsigned int iterator = 0;
-    hash_node_t *temp;
+	unsigned long int index = 0;
+	unsigned int iterator = 0;
+	hash_node_t *temp;
 
-    if(!ht)
-    {
-        return;
-    }
-    printf("{");
-    while (index < ht->size)
-    {
-        temp = ht->array[index];
-        while (temp != NULL)
-        {
-            if (iterator == 1)
-            {
-                printf(", ");
-            }
-            printf("'%s': '%s'", temp->key, temp->value);
-            iterator = 1;
-            temp = temp->next;
-        }
-        index++;
-    }
-    printf("}\n");
+	if (!ht)
+		return;
+	printf("{");
+	while (index < ht->size)
+	{
+		temp = ht->array[index];
+		while (temp != NULL)
+		{
+			if (iterator == 1)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", temp->key, temp->value);
+			iterator = 1;
+			temp = temp->next;
+		}
+		index++;
+	}
+	printf("}\n");
 }
