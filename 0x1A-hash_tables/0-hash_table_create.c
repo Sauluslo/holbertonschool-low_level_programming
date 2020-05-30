2c5623d4b371f1a83a0cf8a3dec16187884f72d2
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-05-30 01:34:50 +0000

    new project, correct style betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_table_t *) * size);
-	if (new_table == NULL)
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
