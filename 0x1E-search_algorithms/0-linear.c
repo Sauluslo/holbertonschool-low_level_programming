685adb0bc608fce1924fb133c1432ba04720dfcc
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-08-28 05:42:14 +0000

    fixed code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t idx;
 
+	if(!array)
+		return (-1);
 	for (idx = 0; idx < size; idx++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
